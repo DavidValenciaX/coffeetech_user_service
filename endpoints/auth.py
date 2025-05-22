38c91ac51048b2ce8d3d55bc9121215f12996c99
DavidValencia526 <davidvalencia0526@gmail.com>
2025-05-22 02:50:45 -0500

    chore: add pytest and pytest-cov dependencies for testing

--- endpoints/auth.py
+++ endpoints/auth.py
@@ -39,7 +39,7 @@ def validate_password_strength(password: str) -> bool:
     if (len(password) >= 8 and
         re.search(r'[A-Z]', password) and
         re.search(r'[a-z]', password) and
-        re.search(r'[0-9]', password) and
+        re.search(r'\d', password) and
         re.search(r'[\W_]', password)):
         return True
     return False
